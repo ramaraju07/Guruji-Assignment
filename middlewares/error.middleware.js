26fbd457592930812e2d1292f80407567e9a6e0b
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2023-06-02 21:44:17 +0530

    error middleware added

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,3 +1,19 @@
 const notFound = (req, res, next) => {
-    
+  const error = new Error(`Path not found ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.send({
+    success: true,
+    message: err.message,
+  });
+};
+
+module.exports = {
+  notFound,
+  errorHandler,
 };
--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { notFound, errorHandler } = require('../middlewares/error.middleware');
 
 app.use(cors());
-app.use(express.json({limit: "50mb"}));
+app.use(express.json({ limit: '50mb' }));
 
-app.get("/", (req, res) =>{
-    res.send({
-        success: true,
-        message: "APP IS WORKING FINE!!"
-    })
-})
+app.get('/', (req, res) => {
+  res.send({
+    success: true,
+    message: 'APP IS WORKING FINE!!',
+  });
+});
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
